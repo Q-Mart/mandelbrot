3c6807de63134a3f8d799d24b4f68f224e8d7df8
Q-Mart <qumarthjash@gmail.com>
2017-07-12 14:50:41 +0100

    Simple print of mandlebrot

--- mandlebrot.C
+++ mandlebrot.C
@@ -21,18 +21,26 @@ bool inMandleBrot(double x, double y)
 
 int main()
 {
-  double x = 0;
-  double y = 0;
+  double x = -2.0;
+  double y = -1.25;
 
-  for (x; x < 100000; x++)
+  double xStep = (0.5-x)/50;
+  double yStep = (1.25-y)/50;
+
+  for (x; x < 0.5; x=x+xStep)
   {
-    for (y; y < 100000; y++)
+    for (y; y < 1.25; y=y+yStep)
     {
+      /* std::cout << "x: " << x << " y: " << y << '\n'; */
       if (inMandleBrot(x, y))
       {
-        std::cout << "x: " << x << " y: " << y << '\n';
+        /* std::cout << "x: " << x << " y: " << y << '\n'; */
+        std::cout << '.';
       }
+      std::cout << ' ';
     }
+    std::cout << '\n';
+    y = -1.25;
   }
 
   return 0;
